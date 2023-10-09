c08f92152f18cf6aa5a934a321b0dec96abf6361
Slade <128598022+SladeGovender@users.noreply.github.com>
2023-10-09 16:12:38 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create an instance of EstateAgentSales
+        EstateAgentSales agent = new EstateAgentSales("Joe Bloggs", 1200000.0);
+
+        // Print the property report
+        agent.printPropertyReport();
+    }
+}
